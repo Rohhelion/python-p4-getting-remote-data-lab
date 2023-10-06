acdab28b689ea11e8231ac9d62b7df2f161cff45
Rohhelion <kirorei04@gmail.com>
2023-10-06 14:09:04 +0300

    done with the lab

--- lib/testing/get_requester_test.py
+++ lib/testing/get_requester_test.py
@@ -1,16 +1,15 @@
-from GetRequester import GetRequester
-class GetRequesterTest:
-    '''Class {Classname} in {modulename}.py'''
+import json
+from lib.get_requester import GetRequester
+
 URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
-JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
-CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
+JSON_STRING = '[{"name": "Daniel", "occupation": "LG Fridge Salesman"}, {"name": "Joe", "occupation": "WiFi Fixer"}, {"name": "Avi", "occupation": "DJ"}, {"name": "Howard", "occupation": "Mountain Legend"}]'
 
 def test_get_response():
-        '''get_response_body function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.get_response_body() == JSON_STRING)
-
-def test_load_json():
-        '''load_json function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.load_json() == CONVERTED_DATA)
+    '''get_response_body function returns response.'''
+    requester = GetRequester(URL)
+    response = requester.get_response_body()
+    
+    # Convert the response (bytes) to a string
+    response_str = response.decode('utf-8')
+    
+    assert response_str == JSON_STRING
